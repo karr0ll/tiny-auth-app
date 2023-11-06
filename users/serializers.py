52b8db3ff5d42c1e04cef40a721de4ef9ba1974a
Kirill Tuchkin <volkirtu@gmail.com>
2023-11-06 22:02:24 +0300

    auth logic added

--- users/serializers.py
+++ users/serializers.py
@@ -1,28 +1,29 @@
 from datetime import datetime
 
-import jwt
 from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainSerializer
 
+from config.validators import PhoneNumberValidator
 from users.models import User
 from users.service.code_generators import generate_invite_code, generate_auth_code
 
 from users.service.code_sender import send_auth_code
 
 
-class UserCreateSerializer(serializers.ModelSerializer, TokenObtainSerializer):
+class UserCreateSerializer(serializers.ModelSerializer):
     phone = serializers.CharField(max_length=16)
     invite_code = serializers.CharField(
         max_length=6,
         required=False,
         write_only=True
     )
-    password = serializers.CharField(
+    auth_code = serializers.CharField(
         max_length=4,
         required=False,
         write_only=True
     )
-    password_created_at = serializers.HiddenField(default=datetime.now(), write_only=True)
+    auth_code_created_at = serializers.HiddenField(default=datetime.now(), write_only=True)
+    id = serializers.IntegerField
 
     class Meta:
         model = User
@@ -31,19 +32,20 @@ class UserCreateSerializer(serializers.ModelSerializer, TokenObtainSerializer):
             'phone',
             'invite_code',
             'is_authenticated',
-            'password',
-            'password_created_at'
+            'auth_code',
+            'auth_code_created_at',
         )
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['password'].required = False
+        validators = [
+            PhoneNumberValidator(field='phone'),
+
+        ]
 
     def create(self, validated_data):
         phone = validated_data['phone']
 
         invite_code = generate_invite_code()
-        password = generate_auth_code()
+        auth_code = generate_auth_code()
 
         queryset = User.objects.filter(phone=phone)
 
@@ -52,46 +54,36 @@ class UserCreateSerializer(serializers.ModelSerializer, TokenObtainSerializer):
         if not queryset.exists():
             user = User.objects.create(
                 phone=phone,
-                password=password
+                auth_code=auth_code
             )
-            send_auth_code(password)
+            send_auth_code(auth_code)
             user.save()
-
         for item in queryset:
             try:
-                password = validated_data['password']
+                auth_code = validated_data['auth_code']
                 # Проверка введенного кода.
                 # Если пользователь не проходил аутентификацию прежде -
                 # генерируется invite code.
-                if password == item.auth_code and not item.is_authenticated:
-
+                if auth_code == item.auth_code and not item.is_authenticated:
                     queryset.update(
                         phone=phone,
                         invite_code=invite_code,
                         is_authenticated=True
                     )
+                    validated_data['id'] = item.pk
+
                 # Проверка введенного кода.
                 # Если пользователь уже проходил аутентификацию -
                 # в базу никакие изменения не вносятся.
-                elif password == item.password and item.is_authenticated:
+                elif auth_code == item.auth_code and item.is_authenticated:
                     queryset.update(
                         phone=phone
                     )
                 else:
-                    queryset.update(
-                        is_authenticated=False
-                    )
                     raise serializers.ValidationError('Authorization code is not valid')
             except KeyError:
-                send_auth_code(password)
+                send_auth_code(auth_code)
                 User.objects.update(
-                    password=password
+                    auth_code=auth_code
                 )
-        print(validated_data)
         return validated_data
-
-    def validate(self, attrs):
-        print(attrs)
-        attrs.update({'password': ''})
-        return super(UserCreateSerializer, self).validate(attrs)
-
