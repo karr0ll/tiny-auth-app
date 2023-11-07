7fe38a8fda1b5695d288a3fbecc24ab46b5a1d67
Kirill Tuchkin <volkirtu@gmail.com>
2023-11-08 00:48:52 +0300

    CreateAPIView code changed

--- users/views.py
+++ users/views.py
@@ -2,7 +2,6 @@ from rest_framework import status
 from rest_framework.generics import CreateAPIView
 from rest_framework.response import Response
 
-from users.models import User
 from users.serializers import UserCreateSerializer
 
 
@@ -12,24 +11,27 @@ class UserCreateView(CreateAPIView):
     """
     serializer_class = UserCreateSerializer
 
-    def create(self, request, *args, **kwargs):
-        super().create(request, *args, **kwargs)
-        serializer = self.get_serializer(data=request.data)
-        if serializer.is_valid():
-            phone = serializer.initial_data['phone']
-            uid = int(User.objects.get(phone=phone).id)
-            try:
-                serializer.initial_data['auth_code']
-            except KeyError:
-                return Response(
-                    {
-                        'message': f"Verification code sent",
-                    }, status.HTTP_201_CREATED
-                )
-            else:
-                return Response(
-                    {
-                        'message': f"Phone verified",
-                        'user_id': f"{uid}"
-                    }, status.HTTP_200_OK
-                )
+
+    # def create(self, request, *args, **kwargs):
+    #     # serializer = self.get_serializer(data=request.data)
+    #     context = self.get_serializer_context()
+    #     print(context)
+    #     if serializer.is_valid():
+    #         phone = serializer.initial_data['phone']
+    #         uid = int(User.objects.get(phone=phone).id)
+    #         try:
+    #             serializer.initial_data['password']
+    #         except KeyError:
+    #             return Response(
+    #                 {
+    #                     'message': f"Verification code sent",
+    #                 }, status.HTTP_201_CREATED
+    #             )
+    #         else:
+    #             return Response(
+    #                 {
+    #                     'message': f"Phone verified",
+    #                     'user_id': f"{uid}"
+    #                 }, status.HTTP_200_OK
+    #             )
+
