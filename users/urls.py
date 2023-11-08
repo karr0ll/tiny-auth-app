65c1c298679a7f44d9d00ab4bcc692aa1e7e38af
Kirill Tuchkin <volkirtu@gmail.com>
2023-11-09 00:29:09 +0300

    profile url added

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
 from users.apps import UserConfig
-from users.views import UserCreateView
+from users.views import UserCreateView, UserProfileView
 
 app_name = UserConfig.name
 
 urlpatterns = [
     path('auth/', UserCreateView.as_view(), name='login'),
-    # path('profile/', UserProfileView.as_view(), name='list')
+    path('profile/<int:pk>', UserProfileView.as_view(), name='profile')
 ]
