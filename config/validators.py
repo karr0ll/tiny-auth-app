25d6491f9c5230f1804b4aabe80af3e5ae4df8ad
Kirill Tuchkin <volkirtu@gmail.com>
2023-11-10 21:27:30 +0300

    validators fixed

--- config/validators.py
+++ config/validators.py
@@ -4,6 +4,8 @@ from typing import OrderedDict
 from django.utils import timezone
 from rest_framework.exceptions import ValidationError
 
+from users.models import User
+
 
 class PhoneNumberValidator:
 
@@ -15,34 +17,54 @@ class PhoneNumberValidator:
 
         if not phone[0] == '7':
             raise ValidationError(
-                "Phone number must start with '7'"
+                {'invalid_phone_number': "phone number must start with '7'"}
             )
 
         for i in phone:
             if i.isalpha():
                 raise ValidationError(
-                    "Phone number must contain only digits"
+                    {'invalid_phone_number': "Phone number must contain only digits"}
                 )
 
         int_code: int = int(phone[1:4])
         if not 900 <= int_code <= 999:
             raise ValidationError(
-                "Only mobile phone numbers supported"
+                {'invalid_phone_number': "only mobile phone numbers supported"}
             )
 
 
-class TokenLifeValidator:
+class AuthCodeLifeValidator:
 
     def __init__(self, field: str) -> None:
         self.field: str = field
 
-    def __call__(self, request_data: OrderedDict[str, datetime]) -> None:
-        auth_code_created_at: datetime = request_data.get(self.field)
-        token_life_time = auth_code_created_at + timedelta(minutes=3)
-        if token_life_time < timezone.now():
+    def __call__(self, request_data: OrderedDict[(str, str)]) -> None:
+        phone = request_data.get(self.field)
+        password_created_on: datetime = User.objects.get(phone=phone).password_created_on
+        auth_code_life_time = password_created_on + timedelta(minutes=3)
+        if auth_code_life_time < timezone.now():
             raise ValidationError(
-                "Token expired"
+                {'error_message': 'authorization code expired'}
             )
+
+
+class PasswordValidator:
+
+    def __init__(self, field: str) -> None:
+        self.field: str = field
+
+    def __call__(self, request_data: OrderedDict[str, str]):
+        auth_code = dict(request_data).get(self.field)
+        if len(auth_code) != 4:
+            raise ValidationError(
+                'authorization code must contain 4 digits'
+            )
+
+        if not auth_code.isnumeric():
+            raise ValidationError(
+                'authorization code must contain only digits'
+            )
+
 
 
 
